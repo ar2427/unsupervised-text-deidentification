c29843b1480c8718c5649b385b773c44194b0d7a
Akhil Raj <akr4007@dc-gpu2.med.cornell.edu>
2024-09-08 17:40:19 -0400

    extract_results_analysis_metrics_from_preds_dataframes_with_key_value_pairs.py now handles latest column name containing relevant dict
    merge issues in plot file resolved

--- utils/plot_masking_percentage_vs_val_acc_graph.py
+++ utils/plot_masking_percentage_vs_val_acc_graph.py
@@ -3,29 +3,11 @@ import matplotlib.pyplot as plt
 # Updated data
 # EHR
 BBN_data = [
-<<<<<<< Updated upstream
-    [0.14, 0.9093], [2.26, 0.4598], [2.55, 0.4245], [3.11, 0.4076], [5.15, 0.3545], [7.40, 0.3579], 
-    [8.69, 0.352], [10.73, 0.2654], [12.69, 0.2107], [14.84, 0.1854], 
-    [15.99, 0.1579], [19.02, 0.1038], [17.92, 0.07846], [18.17, 0.07627], [18.42, 0.0719]
-]
-
-# Deidentify
-BLC_data = [
-    [1.27, 0.8132], [1.29, 0.819],[1.38, 0.7646], [1.71, 0.7971], [2.12, 0.7958], [2.49, 0.7504], 
-    [2.92, 0.7675], [3.80, 0.7298], [5.29, 0.7214], [7.47, 0.5472], 
-    [8.61, 0.5185], [11.28, 0.3752], [12.88, 0.3852], [14.76, 0.2551], [17.98, 0.1436]
-]
-
-# Extracting x and y values
-x1, y1 = zip(*BBN_data)
-x2, y2 = zip(*BLC_data)
-=======
 [0.2065, 0.07627],
 [0.0263, 0.4598],
 [0.2092, 0.0719],
 [0.0297, 0.4245],
 [0.2036, 0.07846],
-# [0.9993, 0.02251],
 [0.0016, 0.9093],
 [0.1121, 0.2654],
 [0.1318, 0.2107],
@@ -83,22 +65,15 @@ PHILTER_data.sort()
 x1, y1 = zip(*BBN_data)
 x2, y2 = zip(*BLC_data)
 x3, y3 = zip(*PHILTER_data)
->>>>>>> Stashed changes
 
 # Plotting the data
 plt.figure(figsize=(10, 6))
 plt.plot(x1, y1, marker='o', label='BBN Tool', linestyle='-', color='red')
 plt.plot(x2, y2, marker='s', label='BLC Tool', linestyle='--', color='blue')
-<<<<<<< Updated upstream
-
-# Adding titles and labels
-plt.title('Comparative Graph of BBN Tool and BLC Tool')
-=======
 plt.plot(x3, y3, marker='x', label='PHILTER Tool', linestyle=':', color='green')
 
 # Adding titles and labels
 plt.title('Comparative Graph of BBN Tool, BLC Tool and PHILTER Tool')
->>>>>>> Stashed changes
 plt.xlabel('% Text masked')
 plt.ylabel('Reidentification accuracy %')
 plt.legend()
